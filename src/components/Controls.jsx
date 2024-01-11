3cf8cdb8420e8dff11ee540429ca0b3289450c7b
Max <ppmax@bk.ru>
2024-01-11 15:27:53 +0300

    Custom selector finished

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,12 +1,43 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
 import Search from './Search';
+import CustomSelect from './CustomSelect';
+
+const options = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
+];
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media (min-width: 767px) {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
 
 const Controls = () => {
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState('');
+  const [region, setRegion] = useState('');
   return (
-    <div>
-      <Search search={search} />
-    </div>
+    <Wrapper>
+      <Search search={search} setSearch={setSearch} />
+      <CustomSelect
+        options={options}
+        placeholder="Filter by region"
+        isClearable
+        isSearchable={false}
+        value={region}
+        onChange={setRegion}
+      />
+    </Wrapper>
   );
 };
 
--- src/components/CustomSelect.jsx
+++ src/components/CustomSelect.jsx
@@ -5,9 +5,50 @@ const CustomSelect = styled(Select).attrs({
   styles: {
     control: (provided) => ({
       ...provided,
+      backgroundColor: 'var(--colors-ui-base)',
+      color: 'var(--colors-text)',
+      borderRadius: 'var(--radius)',
+      padding: '0.25rem',
+      border: 'none',
+      boxShadow: 'var(--shadow)',
+      height: '50px',
     }),
-    option: () => ({}),
+    option: (provided, state) => ({
+      ...provided,
+      cursor: 'pointer',
+      color: 'var(--colors-text)',
+      backgroundColor: state.isSelected
+        ? 'var(--colors-bg)' : 'var(--colors-ui-base)',
+    }),
+    menuList: (provided) => ({
+      ...provided,
+      paddingTop: 0,
+      paddingBottom: 0,
+      borderRadius: 'var(--radius)',
+      backgroundColor: 'var(--colors-ui-base)',
+    }),
   },
-})``;
+})`
+  width: 200px;
+  border-radius: var(--radius);
+  font-family: var(--family);
+  border: none;
+
+  & > * {
+    box-shadow: var(--shadow);
+  }
+
+  & input {
+    padding-left: 0.25rem;
+  }
+
+  & * {
+    color: var(--colors-text) !important;
+  }
+
+  & > div[class*="menu"] {
+    background-color: var(--colors-ui-base);
+  }
+`;
 
 export default CustomSelect;
